89524a3a0c011e9f4541a89ecdf0dbb955fc9a83
Bernhard Brunner <bernhard.brunner@sbb.ch>
2023-04-24 07:50:04 +0100

    initial data

---
+++ src/main/java/ch/elbernito/simple/webservice/frontend/SimpleWebserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.elbernito.simple.webservice.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebserviceBackendApplication.class, args);
+	}
+
+}
